d9ef67c4bdb9de02fe6ee5ebd7bc6a2666e2d163
mohammadreza memar <mohammadrezamemar@mohammarezasMBP.attlocal.net>
2023-01-15 12:15:14 -0600

    re-work on project / trying to improve my skills

--- client/src/js/database.js
+++ client/src/js/database.js
@@ -13,9 +13,24 @@ const initdb = async () =>
   });
 
 // TODO: Add logic to a method that accepts some content and adds it to the database
-export const putDb = async (content) => console.error('putDb not implemented');
+export const putDb = async (content) => {
+  const jateDb = await openDB("jate", 1)
+  const editorText = jateDb.transaction("jate", "readwrite")
+  const save = editorText.objectStore("jate")
+  const request = save.put({ content: content})
+  const result = await request
+  console.log("******* PUT DB ******", result)
+
+}
 
 // TODO: Add logic for a method that gets all the content from the database
-export const getDb = async () => console.error('getDb not implemented');
+export const getDb = async () => {
+  const jateDb = await openDB("jate", 1)
+  const textRetrive = jateDb.transaction("jate", "readonly")
+  const save = textRetrive.objectStore("jate")
+  const request = save.getAll();
+  const result = await request;
+  console.log(">>>>> GET DB <<<<<", result)
+}
 
 initdb();
--- client/webpack.config.js
+++ client/webpack.config.js
@@ -3,75 +3,67 @@ const WebpackPwaManifest = require('webpack-pwa-manifest');
 const path = require('path');
 const { InjectManifest } = require('workbox-webpack-plugin');
 
+// TODO: Add and configure workbox plugins for a service worker and manifest file.
+// TODO: Add CSS loaders and babel to webpack.
+
 module.exports = () => {
   return {
-    mode: 'development',
-    // Entry point for files
+    mode: 'production',
     entry: {
       main: './src/js/index.js',
       install: './src/js/install.js'
     },
-    // Output for our bundles
     output: {
       filename: '[name].bundle.js',
       path: path.resolve(__dirname, 'dist'),
     },
-
-    // Add and configure workbox plugins for a service worker and manifest file.
     plugins: [
-     // Webpack plugin that generates our html file and injects our bundles. 
-     new HtmlWebpackPlugin({
-      // Creates a copy of the index.html file in the dist folder and inserts in script tag to the newly created bundle.js file
-      template: './index.html', 
-      // Optional parameters
-      title: 'Text Editor'
-    }),
-   
-    // Injects our custom service worker
-    new InjectManifest({
-      swSrc: './src-sw.js',
-      swDest: 'src-sw.js',
-    }),
-
-    // Creates a manifest.json file.
-    new WebpackPwaManifest({
-      fingerprints: false,
-      inject: true,
-      name: 'Just another text editor',
-      short_name: 'JATE',
-      description: 'Just another text editor!',
-      background_color: '#225ca3',
-      theme_color: '#225ca3',
-      start_url: '/',
-      publicPath: '/',
-      icons: [
-        {
-          src: path.resolve('src/images/logo.png'),
-          sizes: [96, 128, 192, 256, 384, 512],
-          destination: path.join('assets', 'icons'),
-        },
-      ],
-    }),
+      new HtmlWebpackPlugin({
+        template: "./index.html",
+        title: "Webpack Plugin"
+      }),
+      new InjectManifest({
+        swSrc: "./src-sw.js",
+        swDest: "src-sw.js",
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        name: "text-editor",
+        short_name: "text-editor",
+        description: "text editor that stores text and displays it when you refresh",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "./",
+        publicPath: "./",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("assets", "icons")
+          }
+        ]
+      })
     ],
 
     module: {
-      // Add CSS loaders to webpack
       rules: [
         {
-          // This looks for a .css file and adds this into the bundle.js file
+          test: /\.(png|svg|jpg|jpeg|gif)$/i,
+          type: 'asset/resource',
+        },
+        {
           test: /\.css$/i,
-          // Additional modules for converting the css into js
-          use: ["style-loader", "css-loader"]
+          use: ['style-loader', 'css-loader'],
         },
         {
           test: /\.m?js$/,
           exclude: /node_modules/,
-          // Add babel-loader to webpack in order to use ES6
           use: {
-            loader: "babel-loader",
+            loader: 'babel-loader',
             options: {
-              presets: ["@babel/preset-env"],
-              plugins: ["@babel/plugin-proposal-object-rest-spread", "@babel/transform-runtime"]
+              presets: [
+                ['@babel/preset-env', { targets: "defaults" }]
+              ]
             }
           }
         }
